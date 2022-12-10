6d9c3592feef35abc231553ba8350a739c7af6ee
AngelaPinelo <78389599+AngelaPinelo@users.noreply.github.com>
2022-12-09 21:18:26 -0600

    Ejercicio 7.1

---
+++ Ejercicio7.1/Ejercicio1.py
@@ -0,0 +1,9 @@
+from operaciones import *
+
+num1 = 9 
+num2 = 8
+
+print(suma(num1,num2))
+print(resta(num1,num2))
+print(multi(num1,num2))
+print(division(num1,num2))
---
+++ Ejercicio7.1/operaciones.py
@@ -0,0 +1,17 @@
+
+
+def suma (num1, num2):
+    resultado= num1 + num2
+    return resultado
+
+def resta (num1, num2):
+    resultado= num1 - num2
+    return resultado
+
+def multi (num1, num2):
+    resultado= num1 * num2
+    return resultado
+
+def division (num1, num2):
+    resultado= num1 / num2
+    return resultado
